88cb228f71c601602876583df3d1e98c1d0add59
Simon <simmoe@gmail.com>
2022-08-31 15:36:41 +0200

    api introduction

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //hent biblioteket IP 
 const ip = require('ip')
+//fordi vi har hentet biblioteket IP kan vi hurtoigt få fat på ip adressen
 console.log(ip.address())
 //hent biblioteket express og gem objektet i en konstant
 const express = require('express')
@@ -7,35 +8,27 @@ const express = require('express')
 const app = express()
 //definer en port
 const port = 4040
-//vi laver en meget simpel database 
-const weekDays = {
-    'mandag': 'Jeg har det... mandag.. det er sådan.. nedadgående',
-    'tirsdag': 'Jeg har det godt, det er tirsdag',
-    'onsdag': 'Jeg har det... onsdag, den kan god være svær',
-    'torsdag': 'Torsdag - det er en dejlig dag. Jeg har det fedt',
-    'fredag': 'Ubeskrivelig glad, er jeg',
-    'lørdag': 'Jeg har det roligt, trækker vejret, fuld af LOVE',
-    'søndag': 'Jeg har det okay, det er søndag. Jeg er veludhvilet. Eller.',
-}
 
 //serve en statisk mappe i roden 
 app.use('/', express.static('public'))
 
-
-//start en webserver på port 3000
-app.get('/api/*', (req, res)=>{
-    console.log('Serveren fik besøg i roden')
-    if(req.params[0]){
-        console.log('WOW nogen vil bruge vores api: ' + req.params[0])
-        if(weekDays[req.params[0]]){
-            res.send(weekDays[req.params[0]])
-        }else{
-            res.send(req.params[0] + 'is NOT an api endpoint')
+//Hvis der kommer klienter til endpointet /hvaler
+app.get('/api/hvaler', (req, res)=>{
+    const obj = {
+        'blåhval':{
+            'farve': 'grøn',
+            'vægt':'1/2 mazda'
         }
-    }else{
-        res.send('Du besøgte mig da - i min rod')
     }
+    res.json(obj)
 })
+
+//Hvis der kommer klienter til endpointet /hvaler
+app.get('/api/dato', (req, res)=>{
+    res.send('Du er kommet til mit dato api')
+})
+
+//serveren skal bare sidde og køre på porten
 app.listen(port, ()=>{
     console.log('Server lytter på port: ' + port)
 }) 
--- public/index.js
+++ public/index.js
@@ -1,1 +1,17 @@
 console.log('hejsa')
+let hvalDiv = document.querySelector('#hvaler')
+hvalDiv.style.top = 0
+
+document.querySelector('#hvaler h2').addEventListener('click', ()=> {
+    if(hvalDiv.style.top == '90vh'){
+        hvalDiv.style.top = 0;
+    }else{
+        hvalDiv.style.top = '90vh';
+    }
+})
+
+document.querySelector('#submitH').addEventListener('click', () => {
+    fetch('http://localhost:4040/api/hvaler')
+        .then(res => res.json())
+            .then(json => document.querySelector('#hvaler .json').innerHTML = JSON.stringify(json))
+})
